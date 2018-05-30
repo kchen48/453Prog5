b87ebd199a7b656c0367dfdb8a3d30222a908ad7
Kelly Chen <kchen48@calpoly.edu>
2018-05-29 21:15:58 -0700

    Added basic usage message

--- minget.c
+++ minget.c
@@ -9,6 +9,14 @@
 
 
 int main(int argc, char *argv[]){
+   if (argc == 1){
+      printf("usage: minget [ -v ] [ -p num [ -s num] ] imagefile srcpath [ dstpath ]\n\
+      Options:\n\
+      -p part    --- select partition for filsystem (default: none)\n\
+      -s sub     --- select subpartition for filesystem (default: none)\n\
+      -h help    --- print usage information and exit\n\
+      -v verbose --- increase verbosity level\n");
+   }
 
    return 0;
 }
--- minls.c
+++ minls.c
@@ -8,6 +8,14 @@
 #include "min.h"
 
 int main(int argc, char *argv[]){
+   if (argc == 1){
+      printf("usage: minls [ -v ] [ -p num [ -s num] ] imagefile [ path ]\n\
+      Options:\n\
+      -p part    --- select partition for filsystem (default: none)\n\
+      -s sub     --- select subpartition for filesystem (default: none)\n\
+      -h help    --- print usage information and exit\n\
+      -v verbose --- increase verbosity level\n");
+   }
 
    return 0;
 }
